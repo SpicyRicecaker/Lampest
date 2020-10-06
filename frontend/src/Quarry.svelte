838672b51d9b6e4a6fc1066b09e9c09fe76334a6
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-10-06 11:55:54 -0700

    REALLY scuffed expanded terms with delete, need to build API delete fetch request

--- frontend/src/Quarry.svelte
+++ frontend/src/Quarry.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import type { bubble } from 'svelte/internal';
   import Search from './Search.svelte';
 
   interface termType {
@@ -11,22 +12,44 @@
     lang: string;
   }
 
+  interface svelteTermType {
+    term: termType;
+    expanded: boolean;
+  }
+  let reveal: string = '';
+
   let terms: Array<termType> = [];
   let features: Array<any> = [
     { inner: 'Term', style: 'flex: 1' },
     { inner: 'Definition', style: 'flex: 1' },
   ];
+  let termInfo: Array<any> = [
+    { inner: 'Term', style: 'flex: 1' },
+    { inner: 'Definition', style: 'flex: 1' },
+  ];
+
+  let svelteTerms: Array<svelteTermType> = [];
 
   onMount(async () => {
     await createTerm('/api/terms');
   });
+
+  const updateSvelteTerms = (newTerms: Array<termType>) =>
+    new Promise((resolve, reject) => {
+      svelteTerms = [];
+      for (let i = 0; i < newTerms.length; i += 1) {
+        svelteTerms.push({ term: newTerms[i], expanded: false });
+      }
+      resolve();
+    });
 
   const createTerm = async (queryString) => {
     const serverRes = await fetch(queryString);
     switch (await serverRes.status) {
       case 200: {
         const resTerm = await serverRes.json();
-        terms = await resTerm;
+        await updateSvelteTerms(await resTerm);
+        console.log(svelteTerms);
         break;
       }
       default: {
@@ -40,6 +63,25 @@
     console.log(event.detail.params);
     createTerm(event.detail.params);
   };
+
+  // // When a term is clicked
+  // const expandTerm = (e: MouseEvent) => {
+  //   console.log(e.currentTarget);
+  //   if (
+  //     (e.currentTarget as HTMLDivElement).contains(
+  //       (e.currentTarget as HTMLDivElement).querySelector('#extra')
+  //     )
+  //   ) {
+  //     (e.currentTarget as HTMLDivElement).removeChild(
+  //       (e.currentTarget as HTMLDivElement).querySelector('#extra')
+  //     );
+  //   } else {
+  //     const t = document.createElement('div');
+  //     t.setAttribute('id', 'extra');
+  //     t.innerHTML = '123123123123';
+  //     (e.currentTarget as HTMLDivElement).appendChild(t);
+  //   }
+  // };
 </script>
 
 <style lang="scss">
@@ -66,6 +108,9 @@
       background-color: white;
       text-align: center;
       // box-shadow: 0rem 0.1rem 0.3rem 0.1rem lightgray;
+    }
+    &:hover {
+      cursor: pointer;
     }
   }
 
@@ -98,12 +143,22 @@
     {#each features as feature}<span>{feature.inner}</span>{/each}
   </div>
   <div class="bigwrap">
-    {#each terms as term}
-      <div class="smallwrap">
-        <span class="tail">{term.content}</span>
+    {#each svelteTerms as st, i}
+      <div class="smallwrap" on:click={() => (st.expanded = !st.expanded)}>
+        <span class="tail">{st.term.content}</span>
         <div class="divider" />
-        <span class="head">{term.description}</span>
+        <span class="head">{st.term.description}</span>
       </div>
+      {#if st.expanded}
+        {#each Object.entries(st.term) as [termLabel, termValue]}
+          <div>{termLabel}:{termValue}</div>
+        {/each}
+        <button
+          on:click={() => {
+            svelteTerms.splice(i, 1);
+            svelteTerms = svelteTerms;
+          }}>DELETE</button>
+      {/if}
     {/each}
   </div>
 </main>
