c869a65c89cfb2ec0859c16b5905a04199ccd757
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-10-06 21:45:13 -0700

    Production build and quizlet patch

--- frontend/src/Quarry.svelte
+++ frontend/src/Quarry.svelte
@@ -220,7 +220,7 @@
       <button on:click={() => (quizletVisible = !quizletVisible)}>hide</button>
       <br />
       {#each svelteTerms as st}
-        {st.term.content}<br />{st.term.description}
+        {st.term.content}<br />{st.term.description}<br/>
       {/each}
     </div>
   {/if}
